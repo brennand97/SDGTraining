d6322deb6c4f568e93aa747ac520c0c51dda05cb
douglbre <douglbre@oregonstate.edu>
2017-05-18 11:40:15 -0700

    Working on PeoplePro Implementation

---
+++ PeopleProTraining/PeopleProTraining/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeopleProTraining.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public int? EmployeeId { get; set; }
+        public virtual Employee Director { get; set; }
+
+        public virtual ICollection<Building> Buildings { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
