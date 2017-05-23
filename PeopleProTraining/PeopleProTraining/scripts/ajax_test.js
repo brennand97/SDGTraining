6c06fced7d70ab50df3a4160963e31bf40c47b14
douglbre <douglbre@oregonstate.edu>
2017-05-23 16:48:03 -0700

    All data is now retrieved through ajax

--- PeopleProTraining/PeopleProTraining/scripts/ajax_test.js
+++ PeopleProTraining/PeopleProTraining/scripts/ajax_test.js
@@ -4,55 +4,7 @@ $(document).ready(function () {
 });
 
 function loadData() {
-    // Get department data
-    GetData("departments",
-        function (result) {
-            var html = '';
-            $.each(result, function (key, item) {
-                html += '<tr>';
-                html += '<td>' + item.Name + '</td>';
-                html += '</tr>';
-            });
-            $('.tbody-departments').html(html);
-        },
-        function (errormessage) {
-            alert(errormessage.responseText);
-        }
-    );
-    GetData("buildings",
-        function (result) {
-            var html = '';
-            $.each(result, function (key, item) {
-                html += '<tr>';
-                html += '<td>' + item.Name + '</td>';
-                html += '<td>' + item.Address + '</td>';
-                html += '</tr>';
-            });
-            $('.tbody-buildings').html(html);
-        },
-        function (errormessage) {
-            alert(errormessage.responseText);
-        }
-    );
-    GetData("employees",
-        function (result) {
-            var html = '';
-            $.each(result, function (key, item) {
-                html += '<tr>';
-                html += '<td>' + item.FirstName + '</td>';
-                html += '<td>' + item.LastName + '</td>';
-                html += '<td>' + item.Salary + '</td>';
-                html += '<td>' + item.StartDate + '</td>';
-                html += '<td>' + item.DepartmentId + '</td>';
-                html += '<td>' + item.BuildingId + '</td>';
-                html += '</tr>';
-            });
-            $('.tbody-employees').html(html);
-        },
-        function (errormessage) {
-            alert(errormessage.responseText);
-        }
-    );
+    LoadDatabaseData();
 }
 
 function GetData(table, success_func, error_func) {
@@ -64,4 +16,64 @@ function GetData(table, success_func, error_func) {
         success: success_func,
         error: error_func
     });
+}
+
+function LoadDatabaseData() {
+    // Get department data
+    GetData("departments",
+        function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.Id + '</td>';
+                html += '<td>' + item.Name + '</td>';
+                html += '</tr>';
+            });
+            $('.tbody-departments').html(html);
+        },
+        function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    );
+
+    // Get building data
+    GetData("buildings",
+        function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.Id + '</td>';
+                html += '<td>' + item.Name + '</td>';
+                html += '<td>' + item.Address + '</td>';
+                html += '</tr>';
+            });
+            $('.tbody-buildings').html(html);
+        },
+        function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    );
+
+    // Get employee data
+    GetData("employees",
+        function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                date = new Date(parseInt(item.StartDate.substring(item.StartDate.indexOf("(") + 1, item.StartDate.indexOf(")"))));
+                html += '<tr>';
+                html += '<td>' + item.Id + '</td>';
+                html += '<td>' + item.FirstName + '</td>';
+                html += '<td>' + item.LastName + '</td>';
+                html += '<td>' + item.Salary + '</td>';
+                html += '<td>' + date.toUTCString() + '</td>';
+                html += '<td>' + item.DepartmentId + '</td>';
+                html += '<td>' + item.BuildingId + '</td>';
+                html += '</tr>';
+            });
+            $('.tbody-employees').html(html);
+        },
+        function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    );
 }
